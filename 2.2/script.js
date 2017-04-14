987cc031b39f20212698fe28c20ecafe50bcfe47
mac <mac@macbook-pro-mac.home>
2017-04-14 16:12:29 +0200

    done

---
+++ 2.2/script.js
@@ -0,0 +1,43 @@
+var movies = [
+  {
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'Film o czarodzieju',
+    src: './img/HP.jpg'
+  },
+  {
+    id: 2,
+    title: 'Król Lew',
+    desc: 'Film o królu sawanny',
+    src: './img/KL.jpg'
+  },
+  {
+    id: 3,
+    title: 'Gdzie Jest Nemo',
+    desc: 'Film o zagubionej rybce',
+    src: './img/GJN.jpg'
+  },
+  {
+    id:4,
+    title: 'Gumisie',
+    desc: 'Bajka o dopingu',
+    src: './img/G.jpg'
+  }
+];
+
+var moviesElements = movies.map(function(movie) {
+  return React.createElement('li', {key: movie.id},
+      React.createElement('h2', {}, movie.title),
+      React.createElement('p', {}, movie.desc),
+      React.createElement('img',{src: movie.src}
+  )
+    );
+});
+
+var element =
+  React.createElement('div', {}, 
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements)
+  );
+
+ReactDOM.render(element,document.getElementById('app'));
